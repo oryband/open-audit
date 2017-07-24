e29b5b88b119fd703b06f3792a35b7b6f8e35ded
Ory Band <oryband@gmail.com>
2017-07-24 22:12:02 +0300

    index.html WIP
    base and search templates WIP

--- website/app/app.py
+++ website/app/app.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
 @app.route('/')
 @app.route('/index.html')
 def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def search():
+    print(request.form)
     return render_template('index.html')
